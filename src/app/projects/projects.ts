ec162dc820ba6368f125087acf27d62887264450
Pierce Jordan <piercejord@gmail.com>
2026-05-12 21:53:27 +0100

    PJ - Fix links

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -20,10 +20,10 @@ export class Projects {
   filter = signal<'all' | 'html-css' | 'wordpress'>('all');
 
   projects: ProjectItem[] = [
-    { id: 1, title: 'Joinery Website', category: 'wordpress', image: '/projects/1.webp', viewLink: 'https://www.lohanjoinery.ie', codeLink: '' },
-    { id: 2, title: 'E-commerce Website', category: 'wordpress', image: '/projects/2.webp', viewLink: 'https://4allyouroccasions.ie/', codeLink: '' },
+    { id: 1, title: 'Joinery Website', category: 'wordpress', image: '/projects/1.webp', viewLink: 'https://www.lohanjoinery.ie', codeLink: 'https://github.com/athanri/' },
+    { id: 2, title: 'E-commerce Website', category: 'wordpress', image: '/projects/2.webp', viewLink: 'https://4allyouroccasions.ie/', codeLink: 'https://github.com/athanri/' },
     { id: 3, title: 'Cancer Care Website', category: 'html-css', image: '/projects/3.webp', viewLink: 'https://www.necret.ie/', codeLink: 'https://github.com/athanri/NECRET' },
-    { id: 4, title: 'Carpentry Services', category: 'wordpress', image: '/projects/4.webp', viewLink: 'https://galwaycarpenters.ie/', codeLink: '' },
+    { id: 4, title: 'Carpentry Services', category: 'wordpress', image: '/projects/4.webp', viewLink: 'https://galwaycarpenters.ie/', codeLink: 'https://github.com/athanri/' },
     { id: 5, title: 'Euro American Cancer Forum', category: 'html-css', image: '/projects/5.webp', viewLink: 'https://euroamericanforumoncancer.org/', codeLink: 'https://github.com/athanri/aicri' },
     { id: 6, title: 'Tribes Basketball', category: 'html-css', image: '/projects/6.webp', viewLink: 'https://athanri.github.io/TribesBasketballClub/', codeLink: 'https://github.com/athanri/TribesBasketballClub' },
   ];

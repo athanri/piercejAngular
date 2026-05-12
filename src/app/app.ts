ba224f1f0a5c5e4dc6b16a463f4b93fd3325e3e4
Pierce Jordan <piercejord@gmail.com>
2026-05-12 13:12:24 +0100

    PJ - Added NAvbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet } from '@angular/router';import { NavbarComponent } from './navbar/navbar';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
